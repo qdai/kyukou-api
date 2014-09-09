fda88dd0c9a6dfe1827b10bcef773b4c921be31c
kyudaitaro <kyudai_taro@yahoo.co.jp>
2014-09-09 16:55:00 +0900

    v0.4.0: add RSS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var cronlog = require('./routes/log');
+var rss = require('./routes/rss');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/log', cronlog);
+app.use('/rss', rss);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/rss.js
+++ routes/rss.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var Sequelize = require('sequelize');
 var async = require('async');
 var config = require('../secret/config');
+var RSS = require('rss');
 
 // connect to db
 var sequelize = new Sequelize(config.db_database, config.db_user, config.db_password, {
@@ -37,7 +38,7 @@ router.get('/', function(req, res) {
           events[i].datetime = events[i].date.toISOString();
           events[i].date.setHours(events[i].date.getHours() - new Date().getTimezoneOffset() / 60);
           events[i].dateformatted = events[i].date.getFullYear() + '年' + (events[i].date.getMonth() + 1) + '月' + events[i].date.getDate() + '日（' + ['日','月','火','水','木','金','土'][events[i].date.getDay()] + ')';
-          events[i].date.setHours(events[i].date.getHours() + new Date().getTimezoneOffset() / 60);
+          //events[i].date.setHours(events[i].date.getHours() + new Date().getTimezoneOffset() / 60);
           events[i].department = events[i].department + '学部';
           if (events[i].grade) {
             events[i].grade = parseInt(events[i].grade) ? parseInt(events[i].grade) + '年' : events[i].grade;
@@ -55,6 +56,15 @@ router.get('/', function(req, res) {
       res.render('index', { title: JSON.stringify(err) });
       return;
     }
+    var feed = new RSS({
+      title: config.site.name,
+      description: config.site.description,
+      genarator: config.package.name,
+      feed_url: config.site.url + '/rss',
+      site_url: config.site.url,
+      language: config.site.lang,
+      ttl: 180
+    });
     events.sort(function (a, b) {
       var diff = a.date.getTime() - b.date.getTime();
       if (diff > 0) return 1;
@@ -64,36 +74,19 @@ router.get('/', function(req, res) {
       if (diff < 0) return -1;
       return 0;
     });
-    var columns = {
-      about: [],
-      period: [],
-      department: []
-    };
-    for (var i = 0; i < events.length; i++) {
-      if (columns.about.indexOf(events[i].about) === -1) {
-        columns.about.push(events[i].about);
-      }
-      /*if (columns.period.indexOf(events[i].period) === -1) {
-        columns.period.push(events[i].period);
-      }*/
-      if (columns.department.indexOf(events[i].department) === -1) {
-        columns.department.push(events[i].department);
-      }
+    for (var i = 0; i < 20; i++) {
+      console.log(events[i]);
+      feed.item({
+        title: events[i].dateformatted + '【' + events[i].about + '】' + events[i].subject,
+        description: '【' + events[i].about + '】' + events[i].dateformatted + events[i].period + '\n'
+          + events[i].department + events[i].period + '「' + events[i].subject + '」（' + events[i].teacher + '教員）',
+        url: events[i].link,
+        guid: events[i].id + '-' + events[i].date.getTime() + '-' + events[i].period + '-' + escape(events[i].subject),
+        date: events[i].date.toISOString()
+      });
     }
-    columns.about.sort();
-    //columns.period.sort();
-    columns.department.sort();
-    res.render('index', {
-      url: config.site.url,
-      name: config.site.name,
-      title: config.site.name,
-      description: config.site.description,
-      keywords: config.site.keywords,
-      author: config.package.author,
-      columns: columns,
-      events: events,
-      version: 'v' + config.package.version
-    });
+    res.set('Content-Type', 'application/rss+xml');
+    res.send(feed.xml('  '));
   });
 });
 

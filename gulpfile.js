e769e43b49a8d19eb3f05bec0d95d2e4bc293d8d
egy186 <egy186@yahoo.co.jp>
2015-04-25 20:35:10 +0900

    changed internal build commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var apidoc = require('apidoc');
-var BBPromise = require('bluebird');
 var bower = require('bower');
 var config = require('config');
 var del = require('del');
 var fs = require('fs');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var header = require('gulp-header');
 var less = require('gulp-less');
 var mainBowerFiles = require('main-bower-files');
 var minify = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 
-function apiList (apiData) {
+var srcPathBase = './src';
+var destPathBase = './public';
+var apiList = function (apiData) {
   var list = {};
   apiData.forEach(function (el) {
     if (!list[el.group]) {
@@ -21,66 +21,63 @@ function apiList (apiData) {
     list[el.group].push(el.title);
   });
   return list;
-}
+};
 
-gulp.task('install', function () {
-  return new BBPromise(function (resolve, reject) {
-    bower.commands.install().on('log', function (log) {
-      gutil.log('bower', log.id.cyan, log.message);
-    }).on('end', function (installed) {
-      resolve(installed);
-    }).on('error', function (err) {
-      reject(err);
-    });
+gulp.task('clear', function () {
+  del.sync(destPathBase);
+});
+
+gulp.task('bower:install', function (callback) {
+  bower.commands.install().on('end', function () {
+    callback();
+  }).on('error', function (err) {
+    callback(err);
   });
 });
 
-gulp.task('bower', ['install'], function () {
-  var destPath = 'public/lib';
-  del.sync(destPath);
+gulp.task('bower', ['bower:install'], function () {
+  var destPath = srcPathBase + '/static/lib';
   return gulp.src(mainBowerFiles(), { base: 'bower_components' })
     .pipe(gulp.dest(destPath));
 });
 
-gulp.task('build_js', function () {
-  var destPath = 'public/js';
-  del.sync(destPath);
-  return gulp.src('src/js/**/*.js')
+gulp.task('build:js', function () {
+  var destPath = destPathBase + '/js';
+  return gulp.src(srcPathBase + '/js/**/*.js')
     .pipe(header('var SITE_URL = \'//' + config.get('site.url') + '/\';\n'))
     .pipe(uglify())
     .pipe(gulp.dest(destPath));
 });
 
-gulp.task('build_css', function () {
-  var destPath = 'public/css';
-  del.sync(destPath);
-  gulp.src('src/less/**/*.less')
+gulp.task('build:css', function () {
+  var destPath = destPathBase + '/css';
+  gulp.src(srcPathBase + '/less/**/*.less')
     .pipe(less())
     .pipe(minify())
     .pipe(gulp.dest(destPath));
 });
 
-gulp.task('build_static', function () {
-  gulp.src('src/static/**')
-    .pipe(gulp.dest('public'));
+gulp.task('build:static', ['bower'], function () {
+  gulp.src(srcPathBase + '/static/**')
+    .pipe(gulp.dest(destPathBase));
 });
 
-gulp.task('apidoc', function () {
+gulp.task('apidoc', function (callback) {
   var chunk = apidoc.createDoc({
     src: 'api/',
     parse: true,
+    debug: false
   });
-  chunk.data = JSON.parse(chunk.data);
-  chunk.project = JSON.parse(chunk.project);
-  chunk.list = apiList(chunk.data);
-  fs.writeFileSync('api/doc.json', JSON.stringify(chunk));
-  if (typeof chunk === 'object') {
-    gutil.log('apidoc:', gutil.colors.green('Apidoc created... [ ' + chunk.project.name + ' ] '));
-    return BBPromise.resolve(chunk);
+  if (chunk.data && chunk.project) {
+    chunk.data = JSON.parse(chunk.data);
+    chunk.project = JSON.parse(chunk.project);
+    chunk.list = apiList(chunk.data);
+    fs.writeFileSync('api/doc.json', JSON.stringify(chunk));
+    callback();
   } else {
-    return BBPromise.reject(new Error('Execution terminated (set "debug: true" for details. '));
+    callback(new Error('apiDoc execution terminated (set "debug: true" for details).'));
   }
 });
 
-gulp.task('build', ['bower', 'build_js', 'build_css', 'build_static', 'apidoc']);
+gulp.task('build', ['build:js', 'build:css', 'build:static', 'apidoc']);
 gulp.task('default', ['build']);

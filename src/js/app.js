1f3ac69acc31f717f95bbc24964f3d5efa5a8ba6
egy186 <egy186@yahoo.co.jp>
2015-04-26 15:11:49 +0900

    improve settings view

--- src/js/app.js
+++ src/js/app.js
@@ -1,142 +1,8 @@
 /* global angular, SITE_URL */
 
-var kyukouApp = angular.module('kyukouApp', ['kyukouApp.filters', 'ui.bootstrap', 'LocalStorageModule']);
-kyukouApp.factory('eventList', ['$http', '$q', function ($http, $q) {
-  var toJapaneseDateString = function (date) {
-    return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日（' + ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] + '）';
-  };
-  var deferred = $q.defer();
-  $q.all([
-    $http.get(SITE_URL + 'api/1/events/list.json')
-  ]).then(function (results) {
-    var data = results[0].data;
-    var eventsObj = {};
-    var abouts = [];
-    var departments = [];
-    var i;
-    var date = new Date();
-    var today = toJapaneseDateString(date);
-    date.setDate(date.getDate() + 1);
-    var nextday = toJapaneseDateString(date);
-    for (i = 0; i < data.length; i++) {
-      data[i].raw = data[i].raw.replace(/\s+/g, ' ');
-      // datetime
-      data[i].eventDate = new Date(data[i].eventDate);
-      data[i].datetime = data[i].eventDate.toISOString();
-      data[i].dateformatted = toJapaneseDateString(data[i].eventDate);
-      if (data[i].note || data[i].campus || data[i].note) {
-        data[i].hasNote = true;
-      }
-      if (data[i].dateformatted === today) {
-        data[i].dateformatted = '今日';
-        data[i].isToday = true;
-      }
-      if (data[i].dateformatted === nextday) {
-        data[i].dateformatted = '明日';
-      }
-      // push to events
-      if (!eventsObj[data[i].eventDate.getTime()]) {
-        eventsObj[data[i].eventDate.getTime()] = [];
-      }
-      eventsObj[data[i].eventDate.getTime()].push(data[i]);
-      // push to abouts
-      if (abouts.indexOf(data[i].about) === -1) {
-        abouts.push(data[i].about);
-      }
-      //push to departments
-      if (departments.indexOf(data[i].department.match(/^\S*学部/)[0]) === -1) {
-        departments.push(data[i].department.match(/^\S*学部/)[0]);
-      }
-    }
-    var keys = Object.keys(eventsObj).sort();
-    var events = [];
-    var daysEvent;
-    for (i = 0; i < keys.length; i++) {
-      daysEvent = {
-        data: eventsObj[keys[i]]
-      };
-      daysEvent.date = daysEvent.data[0].dateformatted;
-      // push
-      events[i] = daysEvent;
-    }
-    deferred.resolve({
-      events: events,
-      eventsCount: data.length,
-      abouts: abouts.sort(),
-      departments: departments.sort()
-    });
-  }, function (error) {
-    deferred.reject(error);
-  });
-  return deferred.promise;
-}]);
-kyukouApp.controller('eventListCtrl', ['$scope', 'eventList', 'localStorageService', function ($scope, eventList, localStorageService) {
-  $scope.isCollapsed = true;
-
-  $scope.ctrlTmpl = 'kyukou-loading';
-  $scope.error = null;
-
-  $scope.filteredEvents = [];
-  $scope.filteredCount = function () {
-    var count = 0;
-    for (var i = 0; i < $scope.filteredEvents.length; i++) {
-      count += $scope.filteredEvents[i].length;
-    }
-    return count;
-  };
-  // load
-  eventList.then(function (data) {
-    $scope.events = data.events;
-    $scope.eventsCount = data.eventsCount;
-    $scope.abouts = data.abouts;
-    $scope.departments = data.departments;
-
-    $scope.ctrlTmpl = 'kyukou-app';
-    $scope.error = null;
-  }, function (err) {
-    $scope.error = {
-      status: err.status
-    };
-  });
-
-  if (!localStorageService.get('selectedAbouts') || localStorageService.get('selectedAbouts').length === 0) {
-    localStorageService.set('selectedAbouts', ['休講', '補講', '連絡', '教室変更', 'その他', '公務']);
-  }
-  localStorageService.bind($scope, 'selectedAbouts');
-  $scope.setSelectedAbouts = function () {
-    var about = this.about;
-    var index = $scope.selectedAbouts.indexOf(about);
-    if (index !== -1) {
-      $scope.selectedAbouts.splice(index, 1);
-    } else {
-      $scope.selectedAbouts.push(about);
-    }
-    //return false;
-  };
-  $scope.isSelectedAbout = function (about) {
-    return ($scope.selectedAbouts.indexOf(about) !== -1);
-  };
-
-  if (!localStorageService.get('selectedDepartments') || localStorageService.get('selectedDepartments').length === 0) {
-    localStorageService.set('selectedDepartments', ['教育学部', '文学部', '法学部', '理学部', '経済学部']);
-  }
-  localStorageService.bind($scope, 'selectedDepartments');
-  $scope.setSelectedDepartments = function () {
-    var department = this.department;
-    var index = $scope.selectedDepartments.indexOf(department);
-    if (index !== -1) {
-      $scope.selectedDepartments.splice(index, 1);
-    } else {
-      $scope.selectedDepartments.push(department);
-    }
-    //return false;
-  };
-  $scope.isSelectedDepartment = function (department) {
-    return ($scope.selectedDepartments.indexOf(department) !== -1);
-  };
-}]);
 
 var kyukouAppFilters = angular.module('kyukouApp.filters', []);
+
 kyukouAppFilters.filter('aboutFilter', [function () {
   return function (events, selectedAbouts) {
     if (!angular.isUndefined(events) && !angular.isUndefined(selectedAbouts) && selectedAbouts.length > 0) {
@@ -154,13 +20,14 @@ kyukouAppFilters.filter('aboutFilter', [function () {
     }
   };
 }]);
+
 kyukouAppFilters.filter('departmentFilter', [function () {
   return function (events, selectedDepartments) {
     if (!angular.isUndefined(events) && !angular.isUndefined(selectedDepartments) && selectedDepartments.length > 0) {
       var tempEvents = [];
       angular.forEach(selectedDepartments, function (department) {
         angular.forEach(events, function (event) {
-          if (angular.equals(event.department.match(/^\S*学部/)[0], department)) {
+          if (angular.equals(event.department[0], department[0])) {
             tempEvents.push(event);
           }
         });
@@ -170,4 +37,113 @@ kyukouAppFilters.filter('departmentFilter', [function () {
       return events;
     }
   };
+}]);
+
+var kyukouApp = angular.module('kyukouApp', ['kyukouApp.filters', 'ui.bootstrap', 'LocalStorageModule']);
+
+kyukouApp.factory('eventList', ['$http', '$q', function ($http, $q) {
+  var toJapaneseDateString = function (date) {
+    return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日（' + ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] + '）';
+  };
+  var deferred = $q.defer();
+  $http.get(SITE_URL + 'api/1/events/list.json').then(function (result) {
+    var date = new Date();
+    var today = toJapaneseDateString(date);
+    date.setDate(date.getDate() + 1);
+    var nextday = toJapaneseDateString(date);
+    var eventsObj = {};
+    result.data.forEach(function (event) {
+      event.raw = event.raw.replace(/\s+/g, ' ');
+      // datetime
+      event.eventDate = new Date(event.eventDate);
+      event.datetime = event.eventDate.toISOString();
+      event.dateformatted = toJapaneseDateString(event.eventDate);
+      if (event.note || event.campus || event.note) {
+        event.hasNote = true;
+      }
+      if (event.dateformatted === today) {
+        event.dateformatted = '今日';
+        event.isToday = true;
+      }
+      if (event.dateformatted === nextday) {
+        event.dateformatted = '明日';
+      }
+      // push to eventsObj
+      var time = event.eventDate.getTime();
+      if (!eventsObj[time]) {
+        eventsObj[time] = [];
+      }
+      eventsObj[time].push(event);
+    });
+    var keys = Object.keys(eventsObj).sort();
+    var events = [];
+    var daysEvent;
+    for (var i = 0; i < keys.length; i++) {
+      daysEvent = {
+        data: eventsObj[keys[i]]
+      };
+      daysEvent.date = daysEvent.data[0].dateformatted;
+      // push
+      events[i] = daysEvent;
+    }
+    deferred.resolve({
+      events: events,
+      eventsCount: result.data.length
+    });
+  }, function (err) {
+    deferred.reject(err);
+  });
+  return deferred.promise;
+}]);
+
+kyukouApp.service('defaults', function () {
+  this.departments = ['教育学部', '文学部', '法学部', '理学部', '経済学部'];
+  this.abouts = ['休講', '補講', '連絡', '教室変更', 'その他', '公務'];
+});
+
+kyukouApp.controller('eventListCtrl', ['$scope', 'eventList', 'defaults', 'localStorageService', function ($scope, eventList, defaults, localStorageService) {
+  $scope.toggle = function (item, list) {
+    var idx = list.indexOf(item);
+    if (idx > -1) list.splice(idx, 1);
+    else list.push(item);
+  };
+
+  $scope.exists = function (item, list) {
+    return list.indexOf(item) > -1;
+  };
+
+  $scope.isCollapsed = true;
+
+  $scope.ctrlTmpl = 'kyukou-loading';
+  $scope.error = null;
+
+  $scope.eventsCount = 0;
+  $scope.filteredEvents = [];
+  $scope.filteredCount = function () {
+    return $scope.filteredEvents.reduce(function (count, el) {
+      return count + el.length;
+    }, 0);
+  };
+  eventList.then(function (data) {
+    $scope.events = data.events;
+    $scope.eventsCount = data.eventsCount;
+    $scope.ctrlTmpl = 'kyukou-app';
+    $scope.error = null;
+  }, function (err) {
+    $scope.error = {
+      status: err.status
+    };
+  });
+
+  $scope.departments = defaults.departments;
+  if (!localStorageService.get('selectedDepartments') || localStorageService.get('selectedDepartments').length === 0) {
+    localStorageService.set('selectedDepartments', defaults.departments);
+  }
+  localStorageService.bind($scope, 'selectedDepartments');
+
+  $scope.abouts = defaults.abouts;
+  if (!localStorageService.get('selectedAbouts') || localStorageService.get('selectedAbouts').length === 0) {
+    localStorageService.set('selectedAbouts', defaults.abouts);
+  }
+  localStorageService.bind($scope, 'selectedAbouts');
 }]);
